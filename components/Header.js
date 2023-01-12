354ea76855d7d4895833ecb2cad43c2c9d4bb94d
shahank42 <crazykavlogs@gmail.com>
2023-01-12 23:01:43 +0530

    Quick Change

--- components/Header.js
+++ components/Header.js
@@ -2,10 +2,10 @@ import Link from "next/link";
 
 export default function Header() {
     return (
-        <header className="flex flex-col py-9 px-4 bg-gray-700 ">
+        <header className="flex flex-col px-4 bg-gray-700 py-9 ">
             <nav className="flex justify-between">
                 <div>
-                    <Link href="/" className="text-gray-100 text-2xl font-bold ml-3">shahanknet</Link>
+                    <Link href="/" className="ml-3 text-2xl font-bold text-gray-100">shahankNet</Link>
                 </div>
                 <ul className="flex flex-row space-x-4">
                     <li className="text-gray-100"><Link href="/posts">~/posts/</Link></li>
